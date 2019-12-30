cc6ffeb761c807575d074e7a7993fea8de107ad1
Matthew Gonzalez <matthewgonzalez@Matthews-iMac.local>
2019-12-29 19:55:14 -0600

    created game logic

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const cardsInPlay ["queen","queen","king","king"];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+function flipcard(){
+    var cardOne.push ['0'];
+	console.log( "user flipped" + cards[cardId])
+}
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
---
+++ memory-game/js/main.js
@@ -0,0 +1,4 @@
+
+const cardsInPlay ["queen","queen","king","king"];
+var cardOne.push ['0'];
+console.log( "user flipped queen")
